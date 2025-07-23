75bc51112e814c5794052183ac515bc67ff72fbb
eldn <emmanuellastra@gmail.com>
2025-07-23 15:55:13 +0200

    Add prettier and linter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,22 +1,15 @@
 return {
-    "nvim-treesitter/nvim-treesitter", 
-    branch = 'master', 
-    lazy = false, 
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    lazy = false,
     build = ":TSUpdate",
     config = function()
-local config = require("nvim-treesitter.configs")
-config.setup(
-{
-ensure_installed = {"lua", "javascript"},
-highlight = {enable = true},
-indent = {enable = true},
-
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            ensure_installed = { "lua", "javascript" },
+            highlight = { enable = true },
+            indent = { enable = true },
+        }
+        )
+    end,
 }
-
-)
-
-
-    end
-
-}
-
